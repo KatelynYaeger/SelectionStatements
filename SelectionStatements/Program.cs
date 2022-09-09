71916a2cbb6bcef493c42c42d6d9e11695b94fbe
Katelyn Yaeger <knyaeger93@gmail.com>
2022-09-09 16:55:01 -0500

    final selection statements

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi friend! What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi {userName}! Can you guess my favorite number?");
+
+            int favoriteNumber = 3;
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            else if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+
+            else 
+                    {
+                Console.WriteLine("Nevermind, you're no fun.");
+            }
+        }
+    }
+}
+
